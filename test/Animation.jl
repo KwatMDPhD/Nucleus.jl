6d70c2b87bdd961d21c3c556349d9e0cbf84ddda
KwatMDPhD <kwat.me@icloud.com>
2025-04-15 10:39:21 -0600

    test/Animation.

--- test/Animation.jl
+++ test/Animation.jl
@@ -4,10 +4,9 @@ include("_.jl")
 
 # ---- #
 
-for (gi, pn_) in ((
-    joinpath(TE, "_.gif"),
-    (pkgdir(Nucleus, "data", "Animation", "$id.png") for id in 1:2),
-),)
+const DA = pkgdir(Nucleus, "data", "Animation")
+
+for (gi, pn_) in ((joinpath(TE, "_.gif"), (joinpath(DA, "1.png"), joinpath(DA, "2.png"))))
 
     Nucleus.Animation.writ(gi, pn_)
 

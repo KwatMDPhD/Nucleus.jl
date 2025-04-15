d23409206aefc3770dc2162cd8c0f2d5efdbec2f
KwatMDPhD <kwat.me@icloud.com>
2025-04-15 13:32:39 -0600

    test/ColorScheme.

--- test/ColorScheme.jl
+++ test/ColorScheme.jl
@@ -6,15 +6,15 @@ using Nucleus
 
 # ---- #
 
-const CO = Nucleus.ColorScheme.make(Nucleus.ColorScheme.BR_)
+const CO = Nucleus.ColorScheme.make(("#ff0000", "#0000ff"))
 
-const C1 = RGB(1, 0, 0.0)
+const R1 = RGB(1, 0, 0.0)
 
-const C2 = RGB(0, 0, 1.0)
+const R2 = RGB(0, 0, 1.0)
 
-for (id, re) in
-    ((1, RGB(0, 0, 1)), (0.0, C2), (-0.1, C2), (3, RGB(1, 0, 0)), (1.0, C1), (1.1, C1))
+for (nu, re) in
+    ((1, RGB(1, 0, 0)), (2, RGB(0, 0, 1)), (-0.1, R1), (0.0, R1), (1.0, R2), (1.1, R2))
 
-    @test CO[id] === re
+    @test CO[nu] === re
 
 end

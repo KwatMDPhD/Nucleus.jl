f8bf9e98ce38f9483711ce8c1bcbec8b1366a64c
KwatMDPhD <kwat.me@icloud.com>
2025-04-15 21:10:48 -0600

    test/Path.

--- test/Path.jl
+++ test/Path.jl
@@ -6,15 +6,13 @@ include("_.jl")
 
 # ---- #
 
-const PK = pkgdir(Nucleus)
-
-const JL = joinpath(PK, "test", "Path.jl")
+const JL = pkgdir(Nucleus, "test", "Path.jl")
 
 # ---- #
 
-for (pa, re) in ((JL, true), ("nonexistent.file", false))
+for (st, re) in ((JL, true), ("nonexistent.file", false))
 
-    @test Nucleus.Path.is_path(pa, 2) === re
+    @test Nucleus.Path.is_path(st, 2) === re
 
 end
 
@@ -23,18 +21,6 @@ end
 for re in ("Path.jl",)
 
     @test Nucleus.Path.text(JL) === re
-
-end
-
-# ---- #
-
-for (di, re) in (
-    (dirname(PK), "Nucleus.jl/test/Path.jl"),
-    (PK, "test/Path.jl"),
-    (joinpath(PK, "test"), "Path.jl"),
-)
-
-    @test Nucleus.Path.text(JL, di) === re
 
 end
 

dabd2e0bcfd706cb70e8228d41db0329a8341d2d
KwatMDPhD <kwat.me@icloud.com>
2025-04-16 16:11:46 -0600

    test/Table.

--- test/Table.jl
+++ test/Table.jl
@@ -8,7 +8,7 @@ include("_.jl")
 
 # ---- #
 
-for (n2, n1_, n2_, A, re) in ((
+for (st, s1_, s2_, A, re) in ((
     "C1",
     ["R1", "R2"],
     ["C2", "C3", "C4"],
@@ -16,10 +16,10 @@ for (n2, n1_, n2_, A, re) in ((
         1 3 5
         2 4 6
     ],
-    DataFrame("C1" => ["R1", "R2"], "C2" => [1, 2], "C3" => [3, 4], "C4" => [5, 6]),
+    DataFrame("C1" => ["R1", "R2"], "C2" => 1:2, "C3" => 3:4, "C4" => 5:6),
 ),)
 
-    @test Nucleus.Table.make(n2, n1_, n2_, A) == re
+    @test Nucleus.Table.make(st, s1_, s2_, A) == re
 
 end
 
@@ -27,46 +27,48 @@ end
 
 const DA = pkgdir(Nucleus, "data", "Table")
 
-const GZ = joinpath(DA, "1.tsv.gz")
+const GZ = joinpath(DA, "_.tsv.gz")
 
 # ---- #
 
-for (an, c1_, c2, re) in ((Nucleus.Table.rea(GZ), ["hgnc_id"], "symbol", 43840),)
+for (A, st_, st, re) in ((Nucleus.Table.rea(GZ), ["hgnc_id"], "symbol", 43840),)
 
-    @test length(Nucleus.Table.make_dictionary(an, c1_, c2)) === re
+    @test length(Nucleus.Table.make_dictionary(A, st_, st)) === re
 
 end
 
 # ---- #
 
-# TODO: Loop for writ.
+function writ(A)
 
-const TS = joinpath(TE, "_.tsv")
+    ts = joinpath(TE, "_.tsv")
 
-# ---- #
+    Nucleus.Table.writ(ts, A)
 
-for cs in (joinpath(DA, "1.tsv"), GZ)
-
-    a1 = Nucleus.Table.rea(cs)
-
-    Nucleus.Table.writ(TS, a1)
-
-    a2 = Nucleus.Table.rea(TS)
-
-    @test all(id -> isequal(a1[!, id], a2[!, id]), axes(a1, 2))
+    Nucleus.Table.rea(ts)
 
 end
 
 # ---- #
 
-for (xl, sh) in ((joinpath(DA, "1.xlsx"), "HumanSpecific Genes"),)
+for fi in (joinpath(DA, "_.tsv"), GZ)
 
-    a1 = Nucleus.Table.rea(xl, sh)
+    A1 = Nucleus.Table.rea(fi)
 
-    Nucleus.Table.writ(TS, a1)
+    A2 = writ(A1)
 
-    a2 = Nucleus.Table.rea(TS)
+    @test all(id -> isequal(A1[!, id], A2[!, id]), axes(A1, 2))
 
-    @info "TODO" a1 a2
+end
+
+# ---- #
+
+for (xl, sh) in ((joinpath(DA, "_.xlsx"), "HumanSpecific Genes"),)
+
+    A1 = Nucleus.Table.rea(xl, sh)
+
+    A2 = writ(A1)
+
+    @info "TODO" A1 A2
 
 end
